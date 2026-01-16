5689730a183777329e243d3d19a7f89b1919fd94
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2026-01-16 12:40:38 +0530

    hello world default code using cout

--- core/io/helloworld.cpp
+++ core/io/helloworld.cpp
@@ -1,7 +1,33 @@
-#include <cstdio>
-using namespace std;
+// simple hello world program systax
+// #include <cstdio>
+// using namespace std;
 
-int main(){
-    puts("Hello world!!!");
+// int main(){
+//     puts("Hello world!!!");
+//     return 0; // returning integer value as promised
+// }
+
+// another hello world program syntax using cout;
+
+#include <iostream>
+
+int main(int argc, const char * argv[]){
+    // your code goes below
+    std::cout << "hello world! \n";
     return 0;
 }
+
+// above syntax will also work fine and we can use this too.
+// this is the default code given by any IDE
+
+// comment in c language / c++ language
+
+/*
+    multi line
+    comment 
+    is done this way.
+*/
+
+// void nothing(){
+//     puts("hello wrold"); // returning nothing as promised
+// }
